f94dbe6746a5addb9ea6734309eea339e64dfdf3
ousef777 <yousef.alfeeli@gmail.com>
2024-11-13 09:37:16 +0300

    Added cors

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const app = express();
 const postsRoutes = require('./api/posts/posts.routes');
 const connectDb = require('./database');
 const morgan = require('morgan');
+const cors = require('cors');
 
 connectDb();
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(cors());
 app.use('/posts', postsRoutes);
 app.use(postsRoutes.val);
 app.use('*', (req, res, next) => {
